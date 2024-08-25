e2836204c1994d2ae6488c23776a264e124ed1d9
rewinery <rewinery@outlook.com>
2024-08-25 21:42:04 +0800

    compile

---
+++ FastRTS/build/CMakeFiles/FastRTS.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/Buckets.cpp" "CMakeFiles/FastRTS.dir/Buckets.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/Buckets.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/GloVars.cpp" "CMakeFiles/FastRTS.dir/GloVars.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/GloVars.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/IndexablePoint.cpp" "CMakeFiles/FastRTS.dir/IndexablePoint.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/IndexablePoint.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/LogGen_IO.cpp" "CMakeFiles/FastRTS.dir/LogGen_IO.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/LogGen_IO.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/QuadTreeLog.cpp" "CMakeFiles/FastRTS.dir/QuadTreeLog.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/QuadTreeLog.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/Query.cpp" "CMakeFiles/FastRTS.dir/Query.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/Query.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/RTNode.cpp" "CMakeFiles/FastRTS.dir/RTNode.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/RTNode.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/RangeTree.cpp" "CMakeFiles/FastRTS.dir/RangeTree.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/RangeTree.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/TreeNode.cpp" "CMakeFiles/FastRTS.dir/TreeNode.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/TreeNode.cpp.o.d"
+  "/Users/rewinery/Documents/SummerInternship/FastRTS/FastRTS/main.cpp" "CMakeFiles/FastRTS.dir/main.cpp.o" "gcc" "CMakeFiles/FastRTS.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ FastRTS/build/CMakeFiles/FastRTS.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FastRTS.dir/Buckets.cpp.o"
+  "CMakeFiles/FastRTS.dir/Buckets.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/GloVars.cpp.o"
+  "CMakeFiles/FastRTS.dir/GloVars.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/IndexablePoint.cpp.o"
+  "CMakeFiles/FastRTS.dir/IndexablePoint.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/LogGen_IO.cpp.o"
+  "CMakeFiles/FastRTS.dir/LogGen_IO.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/QuadTreeLog.cpp.o"
+  "CMakeFiles/FastRTS.dir/QuadTreeLog.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/Query.cpp.o"
+  "CMakeFiles/FastRTS.dir/Query.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/RTNode.cpp.o"
+  "CMakeFiles/FastRTS.dir/RTNode.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/RangeTree.cpp.o"
+  "CMakeFiles/FastRTS.dir/RangeTree.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/TreeNode.cpp.o"
+  "CMakeFiles/FastRTS.dir/TreeNode.cpp.o.d"
+  "CMakeFiles/FastRTS.dir/main.cpp.o"
+  "CMakeFiles/FastRTS.dir/main.cpp.o.d"
+  "FastRTS"
+  "FastRTS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FastRTS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
